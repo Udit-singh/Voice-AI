0b3bf8dffd9f0a95b49af60d0f2f0ffe6b39f6ed
Udit singh <Udit-singh@users.noreply.github.com>
2021-12-19 12:35:49 +0530

    structure and dependencies added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Voice assistant News application</h1>
     </div>
   );
-}
+};
 
 export default App;
